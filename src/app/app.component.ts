3d2dfd2cdb6586d8b829239a97d494ea1413feba
waqarnaseem <waqar.naseem@informhealth.co.uk>
2024-06-18 12:31:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PDS.SPA';
+  message:string = 'Hello, PDS.SPA';
+  ngOnInit() {
+    window.addEventListener('message', (event) => {
+      // Make sure the message is from a trusted source
+      if (event.origin === 'http://localhost:62536/') {
+        this.message = event.data;
+        console.log('Message received from WebForms:', event.data);
+        // Handle the message
+      }
+    });
+  }
 }
